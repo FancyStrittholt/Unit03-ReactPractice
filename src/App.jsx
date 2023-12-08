645a12a3b22b54d16a41c97727da835588a6af9b
FancyStrittholt <fancypants@DESKTOP-TSFMQAQ>
2023-12-08 14:25:15 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import {puppyList} from './data.js'
+
+function App() {
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log(puppies)
+  return (
+    <>
+      {
+        puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>
+        })
+      }
+    </>
+  )
+}
+
+export default App
